be221a36c097d4a40d7e349046bda9eac5f95801
Harriet <>
2021-05-28 08:34:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Greeting for your program.
+print("Welcome to the Band Name Generator!")
+#User input
+City = input("What's the name of the city you grew up in?\n")
+Pet = input("What is the name of your pet?\n")
+#Combination of the name of their city and pet to be printed.
+print("You could call your band " + City.lower() + " " + Pet)
